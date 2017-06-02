d31195c8f966662da345f7ae406825d3fc856213
jixuedong <jixuedong@163.com>
2017-06-02 10:58:53 +0800

    修改头文件

--- sunflower/resources/views/home/layouts/layout.blade.php
+++ sunflower/resources/views/home/layouts/layout.blade.php
@@ -5,6 +5,9 @@
 <title>p2p网贷平台</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+<meta name="Keywords" content="" />
+  <link rel="stylesheet" href="{{ URL::asset('loan/css/base.css')}}?v=58abdfe48000c" /> 
+  <link rel="stylesheet" href="{{URL::asset('loan/css/index.css')}}?v=58abdfe48000c" /> 
 <link href="{{ URL::asset('/css/common.css') }}" rel="stylesheet" />
 <link href="{{ URL::asset('/css/index.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ URL::asset('/css/about.css') }}" rel="stylesheet" type="text/css">
@@ -12,7 +15,7 @@
 <link href="{{ URL::asset('/css/detail.css') }}" rel="stylesheet" type="text/css">
 <!-- <link href="{{ URL::asset('/css/user.css') }}" rel="stylesheet" type="text/css"> -->
 <!-- <link href="{{ URL::asset('/css/help.css') }}" rel="stylesheet" /> -->
-<link href="{{ URL::asset('/css/users.css') }}" rel="stylesheet" type="text/css"/>
+<link href="{{ URL::asset('/css/users.css') }}" rel="stylesheet" type="text/css"  />
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/jquery.datetimepicker.css') }}"/>
 <script type="text/javascript" src="{{ URL::asset('/js/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ URL::asset('/js/common.js') }}"></script>
@@ -101,8 +104,8 @@ $(document).ready(function() {
       <div class="fn-left logo"> <a class="" href="{{ URL::route('index/index') }}"> <img src="{{ URL::asset('/images/logo.png') }}"  title=""> </a> </div>
       <ul class="top-nav fn-clear">
         <li class="on"> <a href="{{ URL::route('index/index') }}">首页</a> </li>
-        <li> <a href="{{ URL::route('invest/list') }}" class="">我要投资</a> </li>
-        <li> <a href="#" class="">我要借贷</a> </li>
+        <li> <a href="{{ URL::route('invest') }}" class="">我要投资</a> </li>
+        <li> <a href="{{ URL::route('loan/index') }}" class="">我要借贷</a> </li>
         <li> <a href="{{ URL::route('crowdfunding/cflist') }}" class="">参与众筹</a> </li>
         <li> <a href="{{ URL::route('safe/help') }}">安全保障</a> </li>
         <li class="top-nav-safe"> <a href="{{ url('ucenter/myaccount') }}">我的账户</a> </li>
@@ -124,7 +127,7 @@ $(document).ready(function() {
             </dl>
             <dl>
                 <dt>相关业务</dt>
-                <dd><a href="{{ URL::route('invest/list') }}">我要投资</a><a href="#">我要借款</a></dd>
+                <dd><a href="{{ URL::route('invest') }}">我要投资</a><a href="#">我要借款</a></dd>
             </dl>
             <dl>
                 <dt>帮助中心</dt>
